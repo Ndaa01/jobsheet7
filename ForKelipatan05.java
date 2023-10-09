4e56b400079f7c68df35fe3f5aef3515e1486b50
Ndaa01 <ananda.idn12345@gmail.com>
2023-10-09 13:25:31 +0700

    First Modification

--- ForKelipatan05.java
+++ ForKelipatan05.java
@@ -5,6 +5,7 @@ public class ForKelipatan05 {
         Scanner scan = new Scanner(System.in);
 
         int kelipatan, jumlah = 0, counter = 0;
+        double rataRata;
         System.out.print("Masukan Bilangan Kelipatan (1-9) : ");
         kelipatan = scan.nextInt();
         
@@ -14,7 +15,10 @@ public class ForKelipatan05 {
                 counter++;
             }
         }
+        rataRata = (double) jumlah / counter;
+
         System.out.printf("Banyaknya bilangan %d dari 1 sampai 50 adalah %d\n", kelipatan, counter);
         System.out.printf("Total bilangan kelipatan %d dari 1 sampai 50 adalah %d\n", kelipatan, jumlah);
+        System.out.printf("Rata-rata dari seluruh bilangan kelipatan %d adalah: %.2f\n", kelipatan, rataRata);
     }
 }
