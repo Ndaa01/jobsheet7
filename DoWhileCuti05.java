24213aa8bed27c9249cc7ec0697665fdd52785ed
Ndaa01 <ananda.idn12345@gmail.com>
2023-10-09 14:39:01 +0700

    First Stage

---
+++ DoWhileCuti05.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class DoWhileCuti05 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int jatahCuti, jumlahHari;
+        String konfirmasi;
+
+        System.out.print("Jatah cuti: ");
+        jatahCuti = sc.nextInt();
+
+        do {
+            System.out.print("Apakah anda ingin mengambil cuti (y/t)? ");
+            konfirmasi = sc.next();
+
+            if (konfirmasi.equalsIgnoreCase("y")) {
+                System.out.print("Jumlah hari: ");
+                jumlahHari = sc.nextInt();
+
+                if (jumlahHari <= jatahCuti) {
+                    jatahCuti -= jumlahHari;
+                    System.out.println("Sisa Jatah Cuti: " + jatahCuti);
+                } else {
+                    System.out.println("Sisa Jatah Cuti tidak Mencukupi");
+                    break;
+                } 
+            }
+        } while (jatahCuti > 0);
+    }
+}
